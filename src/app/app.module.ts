b995b2524d3be8bf94b2e0a0a10d0958cbdf897a
santiquinterogarcia <santiago.quintero@pragma.com.co>
2022-04-03 22:26:50 -0500

    Sección terminada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ContadorComponent } from './contador/contador.component';
 import { HeroesModule } from './heroes/heores.module';
+import { ContadorModule } from './contador/contador.module';
 
 @NgModule({
-  declarations: [AppComponent, ContadorComponent],
-  imports: [BrowserModule, HeroesModule],
+  declarations: [AppComponent],
+  imports: [BrowserModule, HeroesModule, ContadorModule],
   providers: [],
   bootstrap: [AppComponent],
 })
